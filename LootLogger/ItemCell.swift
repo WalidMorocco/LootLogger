ef1741ab9eb5697b54d80cd5b587c843fe258edc
WalidMorocco <walidamarpro@gmail.com>
2022-03-09 21:21:38 -0500

    Figure 11.2

---
+++ LootLogger/ItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCell.swift
+//  LootLogger
+//
+//  Created by Amar, Walid  on 3/9/22.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell{
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
